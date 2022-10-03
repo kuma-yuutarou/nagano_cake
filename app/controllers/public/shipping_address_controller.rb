b1d46bb8c40e45c3e93983b0824de6764508c1ad
kuma-yuutarou <kumatarou2294@gmail.com>
2022-10-03 12:45:41 +0000

    first Commit

---
+++ app/controllers/public/shipping_address_controller.rb
@@ -0,0 +1,7 @@
+class Public::ShippingAddressController < ApplicationController
+  def index
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,29 @@
 Rails.application.routes.draw do
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
+  devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+    resources :items, only: [:index, :show]
+    resources :customers, only: [:show, :edit, :update, :unsubscribe, :withdrawal]
+    resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
+    resources :orders, only: [:new, :confirm, :complete, :create, :index, :show]
+    resources :shipping_address, only: [:index, :edit, :create, :update, :destroy]
+  end
+
+  namespace :admin do
+    get 'homes/top'
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

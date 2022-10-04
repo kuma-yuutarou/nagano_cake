8f2f8e51080ed8a899b737a2fdda99ad0d3533e3
kuma-yuutarou <kumatarou2294@gmail.com>
2022-10-04 13:29:28 +0000

    shouhinn_index

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,14 +1,28 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
-    @items = Items.new
+    @item = Item.new
+    @genres = Genre.all
   end
 
   def show
   end
 
   def edit
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path(@item.id)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :genre_id, :id, :price, :is_active)
   end
 end

dd124d44b3cd1781555891b53bf383ddf1e203a1
kuma-yuutarou <kumatarou2294@gmail.com>
2022-10-06 12:25:58 +0000

    kaiinitirann

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -13,12 +13,24 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+    @genres = Genre.all
+
   end
 
   def create
     @item = Item.new(item_params)
     @item.save
     redirect_to admin_item_path(@item.id)
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path
+    else
+      render :edit
+    end
   end
 
   private

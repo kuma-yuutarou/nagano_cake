454d9ecd5f865cf1d0ddfa33a226cd66672daf9e
kuma-yuutarou <kumatarou2294@gmail.com>
2022-10-10 11:47:22 +0000

    kaiinnzyouhou

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -4,10 +4,15 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
+    @customers = Customer.find(params[:id])
   end
 
   def edit
+    @customers = Customer.find(params[:id])
+    
   end
+
+  private
 
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :email, :encrypted_password, :postal_code, :address, :telephone_number, :is_active)

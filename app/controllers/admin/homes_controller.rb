e20cd5aed4a96f792b5bab4e010aa3e26110d13c
kuma-yuutarou <kumatarou2294@gmail.com>
2022-10-12 12:45:49 +0000

    tyuumonnrirekiitirann

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,11 @@
 class Admin::HomesController < ApplicationController
   def top
+    @orders = Order.page(params[:page])
+  end
+
+  private
+
+  def order_prams
+    params.require(:order).permit(:customer_id, :postal_code, :address, :name, :shipping_cost, :total_payment, :payment_method, :order_status)
   end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,4 +6,5 @@ class Customer < ApplicationRecord
   #def full_name
     #first_name + last_name
   #end
+  has_many :orders
 end

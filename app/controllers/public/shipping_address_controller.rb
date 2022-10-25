b944da78c4006a9db6861e6e5b9fa44df0b51b41
kuma-yuutarou <kumatarou2294@gmail.com>
2022-10-25 12:27:28 +0000

    shouhinn

--- app/controllers/public/shipping_address_controller.rb
+++ app/controllers/public/shipping_address_controller.rb
@@ -1,17 +1,37 @@
 class Public::ShippingAddressController < ApplicationController
   def index
-    @addresse = Address.new
+    @address = Address.new
+    @addresses = Address.all
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def create
+    @address = Address.new(address_params)
+    @address.save
+    redirect_to public_shipping_address_index_path
   end
 
   def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to public_shipping_address_index_path
+    else
+      render public_shipping_address_index_path
+    end
   end
 
   def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to public_shipping_address_index_path
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:name, :postal_code, :address)
   end
 end

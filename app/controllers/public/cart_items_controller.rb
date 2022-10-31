73feb277720005f285c6055d1b0a810f4a592e4d
kuma-yuutarou <kumatarou2294@gmail.com>
2022-10-31 12:31:05 +0000

    cart_item

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,6 +1,6 @@
 class Public::CartItemsController < ApplicationController
   def index
-
+    @cart_items = current_customer.cart_items.all
   end
 
   def create
@@ -19,11 +19,18 @@ class Public::CartItemsController < ApplicationController
   #@cart_item.amount = @cart_item.amount + CartItem.new(cart_item_params)
 
   def update
-
+    @cart_item = CartItem.find(params[:id])
+    if @cart_item.update(cart_item_params)
+      redirect_to public_cart_items_path
+    else
+      redirect_to public_cart_items_path
+    end
   end
 
   def destroy
-
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to public_cart_items_path
   end
 
   def destroy_all

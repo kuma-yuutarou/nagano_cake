f496cf8c610342ef21138ff760dff1dadcd577f4
kuma-yuutarou <kumatarou2294@gmail.com>
2022-11-06 12:22:22 +0000

    sannkusu

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,14 +4,32 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+    @cart_items = current_customer.cart_items.all
+    @total = 0
+    @order = Order.new(order_params)
+    @order.shipping_cost = 800
+
+    if params[:order][:selected_address] == "0"
+      @order = Order.new(order_params)
+      @order.address = current_customer.address
+      @order.postal_code = current_customer.postal_code
+      @order.name = current_customer.last_name + current_customer.first_name
+    elsif params[:order][:selected_address] == "1"
+      @address = current_customer.addressess.find(params[:address_id])
+      @order.address = @address.address
+    elsif params[:order][:selected_address] == "2"
+    end
 
   end
 
   def complete
+
   end
 
   def create
-
+    @order = Order.new(order_params)
+    @order.save
+    redirect_to public_orders_complete_path
   end
 
   def index
@@ -21,4 +39,11 @@ class Public::OrdersController < ApplicationController
   def show
 
   end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name, :customer_id, :shipping_cost, :total_payment, :order_status)
+  end
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :customer
+  enum payment_method: { credit_card: 0, transfer: 1 }
+  enum order_status: { waiting: 0, confirmation: 1, production: 2, preparing: 3, sent: 4 }
 end

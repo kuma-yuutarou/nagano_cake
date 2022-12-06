b05df4e662374ce003fdc148c71c268b6d50ad6a
kuma-yuutarou <kumatarou2294@gmail.com>
2022-12-06 12:38:36 +0000

    shousai

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,10 +1,20 @@
 class Admin::OrdersController < ApplicationController
   def show
-    @orders = Order.find(params[:id])
-    @order = Order.new
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
     @customer = Customer.find(params[:id])
   end
 
   def update
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+      redirect_to admin_order_path(@order.id)
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:customer_id, :postal_code, :address, :name, :shipping_cost, :total_payment, :payment_method, :order_status)
   end
 end

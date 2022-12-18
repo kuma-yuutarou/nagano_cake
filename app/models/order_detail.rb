e8eb317ca3048c8de28c429caed814d3017b6dc5
kuma-yuutarou <kumatarou2294@gmail.com>
2022-12-18 12:01:13 +0000

    rireki

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,5 @@
 class OrderDetail < ApplicationRecord
    belongs_to :order
    belongs_to :item
+   enum making_status: { productionnotallowed: 0, waitingforproduction: 1, underconstruction: 2, productioncompleted: 3 }
 end
